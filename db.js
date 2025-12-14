d744865303140a4d639b0dd940ab9085c3b76496
Surubyuru <flastersyt7@gmail.com>
2025-12-14 16:38:10 -0300

    Last Sync: 2025-12-14 16:38 (Mobile)

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'impostor-spygamedb-r1nysg',
   port: process.env.DB_PORT || 3306, // Puerto configurable
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'spywere',
   database: process.env.DB_NAME || 'spy_game_db',
   waitForConnections: true,
   connectionLimit: 10,
